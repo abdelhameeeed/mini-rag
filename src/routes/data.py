9eff6b5f5f8df543500086c7ad0a3d9b57cc9b8d
abdelhameeeed <abdelhamed222222@gmail.com>
2024-08-03 19:34:42 +0300

    Remove redundant function calls

--- src/routes/data.py
+++ src/routes/data.py
@@ -46,7 +46,6 @@ async def upload_data(request: Request, project_id: str, file: UploadFile,
             }
         )
 
-    project_dir_path = ProjectController().get_project_path(project_id=project_id)
     file_path, file_id = data_controller.generate_unique_filepath(
         orig_file_name=file.filename,
         project_id=project_id
@@ -83,7 +82,7 @@ async def upload_data(request: Request, project_id: str, file: UploadFile,
 
     return JSONResponse(
             content={
-                "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
+                "signal": result_signal.value,
                 "file_id": str(asset_record.id),
             }
         )
